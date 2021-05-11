548023a204f071d56f2294bfc5c0a5c03204aa0b
Laurenz <laurenz.ebi@gmail.com>
2021-05-11 11:30:44 +0200

    added tests for BinaryOperation eval()

--- parser/NodeTest.java
+++ parser/NodeTest.java
@@ -92,4 +92,44 @@ public class NodeTest {
         assertEquals("(sin(90.0))", e.toString());
         assertEquals(Type.DOUBLE, e.getType());
     }
+    
+    @Test 
+    public void testAdditionEval() {
+        Node e = new Addition(new Literal(5.0), new Literal(6.0));
+        Node a = new Addition(new Literal(3.0), new Literal(3.0));
+        assertEquals(11.0, e.eval(), 0.0);
+        assertEquals(6.0, a.eval(), 0.0);
+    }
+    
+    @Test 
+    public void testSubtractionEval() {
+        Node e = new Subtraction(new Literal(10.0), new Literal(6.0));
+        Node a = new Subtraction(new Literal(3.0), new Literal(4.0));
+        assertEquals(4.0, e.eval(), 0.0);
+        assertEquals(-1.0, a.eval(), 0.0);
+    }
+    
+    @Test 
+    public void testMultiplicationEval() {
+        Node e = new Multiplication(new Literal(5.0), new Literal(6.0));
+        Node a = new Multiplication(new Literal(3.0), new Literal(0.5));
+        assertEquals(30.0, e.eval(), 0.0);
+        assertEquals(1.5, a.eval(), 0.0);
+    }
+    
+    @Test 
+    public void testDivisionEval() {
+        Node e = new Division(new Literal(6.0), new Literal(3.0));
+        Node a = new Division(new Literal(22.5), new Literal(3.0));
+        assertEquals(2.0, e.eval(), 0.0);
+        assertEquals(7.5, a.eval(), 0.0);
+    }
+    
+    @Test 
+    public void testLiteralEval() {
+        Node e = new Literal(5.0);
+        Node a = new Literal(3.0);
+        assertEquals(5.0, e.eval(), 0.0);
+        assertEquals(3.0, a.eval(), 0.0);
+    }
 }
