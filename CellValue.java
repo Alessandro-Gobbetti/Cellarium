5fdbe8082a726c29b8767dd14d49fbc0c09a1063
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2021-05-13 15:01:30 +0200

    removed garbage

--- CellValue.java
+++ CellValue.java
@@ -6,67 +6,10 @@
  * @version (a version number or a date)
  */
 public interface CellValue {
-    
-    
-    // private String stringValue;
-    // private double numberValue;
-    // private String errorValue;
-    // protected CellType type;
-    
-    
-    
        
     public CellType type();
     
     public boolean isConvertibleToNumber();
     
     public double asNumber();
-    
-    // public CellValue() {
-        // type = CellType.EMPTY;
-    // }
-    
-    // public CellType type() {
-        // return type;
-    // }
-    
-    // public double asNumber() {
-        // //FIXME
-        // assert(type() == CellType.NUMBER || type() == CellType.EMPTY);
-        // return type() == CellType.NUMBER ? numberValue : 0.0;
-    // }
-    
-    // public boolean isConvertibleToNumber() {
-        // return type() == CellType.NUMBER || type() == CellType.EMPTY;
-    // }
-    
-    
-    
-    
-    
-    
-    // public static CellValue newEmptyCellValue() {
-        // return new CellValue();
-    // }
-    
-    // public static CellValue newNumberCellValue(double x) {
-        // CellValue c = new CellValue();
-        // c.type = CellType.NUMBER;
-        // c.numberValue = x;
-        // return c;
-    // }
-    
-    // public static CellValue newStringCellValue(String x) {
-        // CellValue c = new CellValue();
-        // c.type = CellType.STRING;
-        // c.stringValue = x;
-        // return c;
-    // }
-    
-    // public static CellValue newErrorCellValue(String x) {
-        // CellValue c = new CellValue();
-        // c.type = CellType.ERROR;
-        // c.errorValue = x;
-        // return c;
-    // }
 }
