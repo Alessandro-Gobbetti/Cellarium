f967701d7994277edcb1db9742f3d95e8bfa670f
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2021-05-13 12:44:31 +0200

    better tests for spreadsheet, working on a better CellValue structure

---
+++ CellType.java
@@ -0,0 +1,13 @@
+
+/**
+ * To describe the different type of a CellValue
+ *
+ * @author Alessandro Gobbetti
+ * @version (version number or date here)
+ */
+public enum CellType {
+    EMPTY, 
+    STRING, 
+    NUMBER, 
+    ERROR
+}
--- SpreadsheetTest.java
+++ SpreadsheetTest.java
@@ -1,9 +1,7 @@
-
-
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * The test class SpreadsheetTest.
