c97ce61402f0db9bb40768a4d852d9808c7cfe47
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2021-05-17 09:54:52 +0200

    fixing style warnings in CellReference.java

--- CellReference.java
+++ CellReference.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * @author Alessandro Gobbetti
  * @version 2021.05.12
  */
-public class CellReference extends Node{
+public class CellReference extends Node {
     private boolean rowIsConstant;
     private int row;
     private boolean colIsConstant;
@@ -19,7 +19,8 @@ public class CellReference extends Node{
      * @param colIsConstant true if the column is constant: i.e. $A1 
      * @param col the column to look at
      */
-    public CellReference(final boolean rowIsConstant, final int row, final boolean colIsConstant, final int col) {
+    public CellReference(final boolean rowIsConstant, final int row,
+                         final boolean colIsConstant, final int col) {
         this.rowIsConstant = rowIsConstant;
         this.row = row;
         this.colIsConstant = colIsConstant;
@@ -27,44 +28,48 @@ public class CellReference extends Node{
     }
     
     /**
-     * FIXME
+     * To convert from Alpha26 notation to integer.
+     * @param codeAlpha26 the string to convert
+     * @return a integer corresponding to the given Alpha26 number
      */
     public static int fromAlpha26(final String codeAlpha26) {
-    int result = 0;
-    final int codeLenght = codeAlpha26.length();
-    // looping the given string code from left to right
-    for (int k = 0; k < codeLenght; ++k) {
-        char c = codeAlpha26.charAt(k);
+        int result = 0;
+        final int codeLenght = codeAlpha26.length();
+        // looping the given string code from left to right
+        for (int k = 0; k < codeLenght; ++k) {
+            char c = codeAlpha26.charAt(k);
             // Character.getNumericValue() returns the values
             //  10-35 for the letter A-Z
             int digit = Character.getNumericValue(c) - 9;
             // digits are multiplied by 26^0, 26^1, ...
             result = result * 26 + digit;
-    }
-    // ALPHA-26 count colum A as the 1st column, we want to have 0 index.
-    return result - 1;
+        }
+        // ALPHA-26 count colum A as the 1st column, we want to have 0 index.
+        return result - 1;
     }
     
     /**
-     * FIXME
+     * To convert from column integer to Alpha26 notation.
+     * @param col the column integer to convert
+     * @return a Alpha26 number corresponding to the given column input
      */
     public static String toAlpha26(final int col) {
         // ALPHA-26 count colum A as the 1st column, we want to have 0 index.
-    int alpha26Col = col + 1;
-    String result = "";
-    // we extract a digit at a time until we have no remainder.
-    while(alpha26Col > 0) {
-        int currentDigit = alpha26Col % 26;
-        if(currentDigit == 0) {
-            currentDigit = 26;
+        int alpha26Col = col + 1;
+        String result = "";
+        // we extract a digit at a time until we have no remainder.
+        while (alpha26Col > 0) {
+            int currentDigit = alpha26Col % 26;
+            if (currentDigit == 0) {
+                currentDigit = 26;
+            }
+            alpha26Col = (alpha26Col - currentDigit) / 26;
+            // convert the digit to the letters A, B, C, ...
+            char colChar = (char)(currentDigit + (int)('A') - 1);
+            // add digit from right to left
+            result = colChar + result;
         }
-        alpha26Col = (alpha26Col - currentDigit) / 26;
-        // convert the digit to the letters A, B, C, ...
-        char colChar = (char)(currentDigit + (int)('A') - 1);
-        // add digit from right to left
-        result = colChar + result;
-    }
-    return result;
+        return result;
     }
     
     @Override
@@ -82,7 +87,8 @@ public class CellReference extends Node{
         if (rowIsConstant) {
             result = result + "$";
         }
-        // ALPHA-26 count colum A as the 1st column, we want to have 0 index. We also count row 0 as the first column.
+        // ALPHA-26 count colum A as the 1st column, we want to have 0 index.
+        // We also count row 0 as the first column.
         final int alpha26Row = row + 1;
         result = result + alpha26Row;
         return result;
@@ -106,6 +112,7 @@ public class CellReference extends Node{
         return colIsConstant ? col : baseCol + col;
     }
     
+    @Override
     public CellValue eval(final Spreadsheet spreadsheet) {
         return spreadsheet.getValue(row, col);
     }
