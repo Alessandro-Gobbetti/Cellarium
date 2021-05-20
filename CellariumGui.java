ac57050bcc540a8a8bda4dcaee430bbc01a5837a
Laurenz <laurenz.ebi@gmail.com>
2021-05-20 12:13:27 +0200

    Worked on GUI

--- CellariumGui.java
+++ CellariumGui.java
@@ -1,85 +1,85 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JTable;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Graphics;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
-import java.awt.Dimension;
-import javax.swing.BoxLayout;
 import javax.swing.UIManager;
 
 /**
  * The Main GUI class.
  *
- * @author (Alessandro Gobbetti & Laurenz Ebi)
- * @version (1.0)
+ * @author Alessandro Gobbetti & Laurenz Ebi
+ * @version 1.0
  */
 public class CellariumGui {
 
     /**
      * Main function to start GUI.
+     * @param args  String[] of commands.
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         //Font style
-        Font font = new Font("calibri", Font.PLAIN, 14);
+        final Font font = new Font("calibri", Font.PLAIN, 14);
         
         UIManager.put("Menu.font", font);
         UIManager.put("MenuItem.font", font);
         UIManager.put("Button.font", font);
 
         // Frame
-        JFrame frame = new JFrame("Cellarium");
+        final JFrame frame = new JFrame("Cellarium");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         
         //MenuBar
-        JMenuBar menubar = new JMenuBar();
+        final JMenuBar menubar = new JMenuBar();
         frame.setJMenuBar(menubar);
         
-        JMenu fileMenu = new JMenu("File");
+        final JMenu fileMenu = new JMenu("File");
         menubar.add(fileMenu);
-        JMenu editMenu = new JMenu("Edit");
+        final JMenu editMenu = new JMenu("Edit");
         menubar.add(editMenu);
-        JMenu helpMenu = new JMenu("Help");
+        final JMenu helpMenu = new JMenu("Help");
         menubar.add(helpMenu);
         
-        JMenuItem openItem = new JMenuItem("Open");
+        final JMenuItem openItem = new JMenuItem("Open");
         fileMenu.add(openItem);
-        JMenuItem quitItem = new JMenuItem("Quit");
+        final JMenuItem quitItem = new JMenuItem("Quit");
         fileMenu.add(quitItem);
-        JMenuItem printItem = new JMenuItem("Print");
+        final JMenuItem printItem = new JMenuItem("Print");
         fileMenu.add(printItem);
-        JMenuItem saveItem = new JMenuItem("Save");
+        final JMenuItem saveItem = new JMenuItem("Save");
         editMenu.add(saveItem);
-        JMenuItem deliteAllItem = new JMenuItem("Delite All");
+        final JMenuItem deliteAllItem = new JMenuItem("Delite All");
         editMenu.add(deliteAllItem);
-        JMenuItem helpItem = new JMenuItem("Cellarium Help");
+        final JMenuItem helpItem = new JMenuItem("Cellarium Help");
         helpMenu.add(helpItem);
         
         
         
         //Panels
-        JPanel mainPanel = new JPanel();
+        final JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
         
         
-        JPanel topPanel = new JPanel();
+        final JPanel topPanel = new JPanel();
         topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.PAGE_AXIS));
         
-        JPanel toolbarPanel = new JPanel();
+        final JPanel toolbarPanel = new JPanel();
         toolbarPanel.setLayout(new FlowLayout());
         
         //Buttons
-        JButton open = new JButton("Open");
+        final JButton open = new JButton("Open");
         toolbarPanel.add(open);
-        JButton save = new JButton("Save");
+        final JButton save = new JButton("Save");
         toolbarPanel.add(save);
         
         topPanel.add(toolbarPanel);
